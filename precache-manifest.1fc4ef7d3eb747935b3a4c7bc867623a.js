e60e49765c2537c006d2306669a6f0a46d516681
김뉴리 <help@theblueeye.com>
2021-04-20 10:36:33 +0900

    Updates

---
+++ precache-manifest.1fc4ef7d3eb747935b3a4c7bc867623a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1787699d979655369cf539ff4f2880",
+    "url": "/firecrud/index.html"
+  },
+  {
+    "revision": "e1006c4a0779e20732e6",
+    "url": "/firecrud/static/css/main.77a84cdb.chunk.css"
+  },
+  {
+    "revision": "87f23d6e7fe744ef932d",
+    "url": "/firecrud/static/js/2.1ffbc310.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/firecrud/static/js/2.1ffbc310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1006c4a0779e20732e6",
+    "url": "/firecrud/static/js/main.ccd2dea0.chunk.js"
+  },
+  {
+    "revision": "a5c5067a082cd1f06502a9485eb2304f",
+    "url": "/firecrud/static/js/main.ccd2dea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af4a762545d1b95e9e4f",
+    "url": "/firecrud/static/js/runtime-main.dc385ae4.js"
+  }
+]);
